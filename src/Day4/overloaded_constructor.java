51c1124324aafb8efbb83802b80d2d5167cf87c1
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-07-13 00:44:17 +0530

    Solved problem: 2026-07-13

--- src/Day4/overloaded_constructor.java
+++ src/Day4/overloaded_constructor.java
@@ -1,4 +1,43 @@
 package Day4;
 
-public class overloaded_constructor {
+class Base1{
+    public Base1(){
+        System.out.println("i m a base class constructor ");
+    }
+    public Base1(int x){
+        System.out.println("im an overloaded constructor with value of a as:"+ x );
+    }
 }
+class Derived1 extends Base1 {
+    Derived1() {
+        //super(0);
+        System.out.println(" im in derived class ");
+    }
+
+    Derived1(int x, int y) {
+        super(x);
+        System.out.println("im an overloaded constructor derived with value of b as:" + y);
+    }
+}
+class ChildOfDerived extends Derived1 {
+    ChildOfDerived() {
+        System.out.println(" im child of derived1 ");
+    }
+
+    ChildOfDerived(int x, int y, int z) {
+        super(x, y);
+        System.out.println(" i am overloaded constructor of derived 1 as value of z:" + z);
+    }
+}
+
+public class overloaded_constructor {
+    public static void main(String[] args) {
+        // Base1 b=new Base1();
+        //  Derived1 d =new Derived1();
+//Derived1 d=new Derived1(45,9);
+        ChildOfDerived cd =new ChildOfDerived(4,8,5);
+    }
+}
+
+
+
