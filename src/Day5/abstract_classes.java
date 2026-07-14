8447cfea21aaf480657fafb6a60bf20064335530
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-07-15 01:08:10 +0530

    Solved problem: 2026-07-15

---
+++ src/Day5/abstract_classes.java
@@ -0,0 +1,45 @@
+package Day5;
+
+abstract class  Parent2 {
+    Parent2() {
+        System.out.println(" i am const");
+    }
+
+    public void sayHello() {
+        System.out.println("hello");
+
+    }
+    abstract public void greet();
+    abstract public void greet1();
+}
+class Child2 extends Parent2 {
+    @Override
+    public void greet() {
+        System.out.println("GM");
+    }
+
+    @Override
+    public void greet1() {
+        System.out.println("GN");
+    }
+}
+// }class Child3 extends Parent2{
+//     public void th(){  // error throws
+//         System.out.println(" i am good");
+//     }
+
+public class abstract_classes {
+    public static void main(String[] args) {
+
+        Parent2 obj=new Child2();
+        //Child2 c=new Child2();
+
+        obj.greet();
+        obj.greet1();
+        obj.sayHello();
+        //parent2 child3 ka obj nhi banega
+    }
+}
+
+
+
