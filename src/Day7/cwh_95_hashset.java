10a3f5eaf1313a4f8e74cef14c084e53d6db1b59
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-07-17 18:30:03 +0530

    Solved problem: 2026-07-17

---
+++ src/Day7/cwh_95_hashset.java
@@ -0,0 +1,19 @@
+package Day7;
+
+import java.util.HashSet;
+
+public class cwh_95_hashset {
+    public static void main(String[] args) {
+        HashSet<Integer> h= new HashSet<>(16,0.75f);
+        h.add(4);
+        h.add(7);
+        h.add(8);
+        h.add(1);
+        System.out.println(h);
+        h.clear();
+        System.out.println(h);
+
+
+
+    }
+}
