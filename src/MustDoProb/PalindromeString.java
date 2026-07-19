0bf28d09aaeadaf9e31fe381c34bfecde791360e
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-07-19 20:56:16 +0530

    Solved problem: 2026-07-19

---
+++ src/MustDoProb/PalindromeString.java
@@ -0,0 +1,19 @@
+package MustDoProb;
+
+public class PalindromeString {
+    static boolean palindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+                left++;
+                right--;
+            }
+        return true;
+        }
+
+    public static void main(String[] args) {
+        System.out.println( palindrome("madam"));;
+
+    }
+}
