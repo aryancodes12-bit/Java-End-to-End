43a7b0bff4213700e79e77429773e19298bdd775
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-07-19 18:17:55 +0530

    Solved problem: 2026-07-19

--- src/MustDoProb/GCD_LCM_optimal.java
+++ src/MustDoProb/GCD_LCM_optimal.java
@@ -1,5 +1,21 @@
 package MustDoProb;
-
+ import java.util.*;
 public class GCD_LCM_optimal {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
 
+        int n1 = scn.nextInt();
+        int n2 = scn.nextInt();
+        int temp1 = n1;
+        int temp2 = n2;
+        while (n1 % n2 != 0) {
+            int rem = n1 % n2;
+            n1 = n2;
+            n2 = rem;
+        }
+        int gcd =n2;
+        int lcm= (temp1*temp2)/gcd;
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
 }
