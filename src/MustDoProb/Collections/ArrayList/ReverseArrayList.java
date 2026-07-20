5bd31f7fb6e269bdd3eced6a75917ba1784eb8e9
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-07-20 19:58:15 +0530

    Solved problem: 2026-07-20

--- src/MustDoProb/Collections/ArrayList/ReverseArrayList.java
+++ src/MustDoProb/Collections/ArrayList/ReverseArrayList.java
@@ -1,4 +1,17 @@
 package MustDoProb.Collections.ArrayList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReverseArrayList {
+    public static void main(String[] args) {
+        List<Integer> s = new ArrayList<>();
+        s.add(45);
+        s.add(78);
+        s.add(14);
+        s.add(6);
+        for (int i = s.size()-1;i>=0; i--) {
+            System.out.println(s.get(i));
+        }
+    }
 }
